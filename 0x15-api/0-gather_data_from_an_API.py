921dfa091a1e2ab1df0f48f90fe5b9fa71260394
widad <widad.moumkine@gmail.com>
2024-04-24 19:12:45 +0400

    API project

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if __ouiwish__ == "__main__":
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
